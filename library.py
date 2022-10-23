712055967a1939b5ffbe5f859ee50d2a17d382c9
daniel jackson <deejack@daniels-MacBook-Pro.local>
2022-10-23 15:04:16 -0400

    challenge12

--- library.py
+++ library.py
@@ -1,23 +1,33 @@
+from turtle import title
 from book import Book
 
 
 class Library():
     def __init__(self):
         """Initialize the empty book list"""
-        pass
+        self.books = []
 
-    def add_title(self):
+    def add_title(self,title,author):
         """Add a Book object with the given title and author to the book list"""
-        pass
+        book = Book(title, author)
+        self.books.append(book)
 
     def count_books(self):
         """Return the number of books currently in the booklist"""
-        pass
+        return len(self.books)
 
     def remove_title(self):
         """Remove a book from the book list"""
-        pass
+        for novel in self.books:
+            if novel.title == title:
+                self.books.remove(novel)
+            return self.books
+
+    def display_books(self):
+        for novel in self.books:
+            print(f'{novel.title} -  {novel.author}')
+
 
     def is_empty(self):
-        """Return True if the book list is empty, False if not"""
+        """Return True  if the book list is empty, False if not"""
         return self.books == []
